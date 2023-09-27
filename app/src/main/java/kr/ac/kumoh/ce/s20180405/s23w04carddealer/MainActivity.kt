809e80b0d696e8fc728edb2bca32caf947aed1d8
MaiT <mytmicekorea@naver.com>
2023-09-27 16:25:41 +0900

    Initial commit

---
+++ app/src/main/java/kr/ac/kumoh/ce/s20180405/s23w04carddealer/MainActivity.kt
@@ -0,0 +1,15 @@
+package kr.ac.kumoh.ce.s20180405.s23w04carddealer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kr.ac.kumoh.ce.s20180405.s23w04carddealer.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var main : ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        main = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(main.root)
+        main.card1.setImageResource(R.drawable.c_ace_of_spades2)
+    }
+}
