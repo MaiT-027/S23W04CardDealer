e6c9dbfee8573c62d3e3440d6a6d932913f09359
MaiT-027 <mytmicekorea@naver.com>
2023-10-04 16:07:39 +0900

    카드 생성 및 표시

--- app/src/main/java/kr/ac/kumoh/ce/s20180405/s23w04carddealer/MainActivity.kt
+++ app/src/main/java/kr/ac/kumoh/ce/s20180405/s23w04carddealer/MainActivity.kt
@@ -2,14 +2,47 @@ package kr.ac.kumoh.ce.s20180405.s23w04carddealer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import kr.ac.kumoh.ce.s20180405.s23w04carddealer.databinding.ActivityMainBinding
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
     private lateinit var main : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         main = ActivityMainBinding.inflate(layoutInflater)
         setContentView(main.root)
-        main.card1.setImageResource(R.drawable.c_ace_of_spades2)
+        val card = Random.nextInt(52)
+        //Log.i("Test", "$card : ${getCardName(card)}")
+
+        val res = resources.getIdentifier(
+            getCardName(card),
+            "drawable",
+            packageName
+        )
+
+        main.card1.setImageResource(res)
+    }
+
+    private fun getCardName(c: Int) : String {
+        val shape = when (c / 13) {
+            0 -> "spades"
+            1 -> "diamonds"
+            2 -> "hearts"
+            3 -> "diamonds"
+            else -> "error"
+        }
+
+        val number = when (c % 13) {
+            0 -> "ace"
+            in 1..9 -> (c % 13 + 1).toString()
+            10 -> "jack"
+            11 -> "queen"
+            12 -> "king"
+            else -> "error"
+        }
+
+        return "c_${number}_of_${shape}"
     }
 }
