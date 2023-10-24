49dd14e42bb86894427e163dcc3aa014b933d689
MaiT-027 <maitmus@gmail.com>
2023-10-24 20:50:33 +0900

    구현 완료

---
+++ app/src/main/java/kr/ac/kumoh/ce/s20180405/s23w04carddealer/CardDealerViewModel.kt
@@ -0,0 +1,121 @@
+package kr.ac.kumoh.ce.s20180405.s23w04carddealer
+
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import kotlin.random.Random
+
+class CardDealerViewModel : ViewModel() {
+    private var _cards = MutableLiveData(IntArray(5) { -1 })
+    private var _rankText: MutableLiveData<String> = MutableLiveData()
+    val cards: LiveData<IntArray>
+        get() = _cards
+    val rankText: LiveData<String>
+        get() = _rankText
+    fun shuffle() {
+        var num: Int
+        val newCards = IntArray(5) { -1 }
+
+        for (i in newCards.indices) {
+            do {
+                num = Random.nextInt(52)
+            } while (newCards.contains(num))
+            newCards[i] = num
+        }
+
+        newCards.sort()
+        _cards.value = newCards
+    }
+
+    fun checkRank() {
+        when {
+            isRoyalStraightFlush() -> _rankText.value = "로얄 스트레이트 플러쉬"
+            isBackStraightFlush() -> _rankText.value = "백스트레이트 플러쉬"
+            isStraightFlush() -> _rankText.value = "스트레이트 플러쉬"
+            isFourCards() -> _rankText.value = "포 카드"
+            isFullHouse() -> _rankText.value = "풀 하우스"
+            isFlush() -> _rankText.value = "플러시"
+            isMountain() -> _rankText.value = "마운틴"
+            isBackStraight() -> _rankText.value = "백 스트레이트"
+            isStraight() -> _rankText.value = "스트레이트"
+            isTriple() -> _rankText.value = "트리플"
+            isTwoPair() -> _rankText.value = "투 페어"
+            isPair() -> _rankText.value = "원 페어"
+            else -> _rankText.value = "탑 카드"
+        }
+    }
+
+    private fun isRoyalStraightFlush(): Boolean {
+        return isMountain() && isFlush()
+    }
+
+    private fun isBackStraightFlush(): Boolean {
+        return isFlush() && isBackStraight()
+    }
+
+    private fun isStraightFlush(): Boolean {
+        return isStraight() && isFlush()
+    }
+
+    private fun isFullHouse(): Boolean {
+        if (isTriple()) {
+            val numbers = _cards.value?.map { it % 13 }?.toList()?.sorted()
+            if (numbers?.toSet()?.size == 2) {
+                return true
+            }
+        }
+        return false
+    }
+
+    private fun isStraight(): Boolean {
+        val numbers = _cards.value?.map { it % 13 }?.toList()?.sorted()
+        Log.i("Straight?", numbers.toString())
+        for (i in 0 until 4) {
+            if (numbers?.get(i)!! + 1 != numbers[i + 1]) return false
+        }
+        return true
+    }
+
+    private fun isMountain(): Boolean {
+        val numbers = _cards.value?.map { it % 13 }?.toList()?.sorted()
+        Log.i("Mountain?", numbers.toString())
+        return numbers == intArrayOf(0, 9, 10, 11, 12).toList()
+    }
+
+    private fun isFlush(): Boolean {
+        val suits = _cards.value!!.map { it / 13 }.toList().sorted()
+        return suits.all { it == suits[0] }
+    }
+
+    private fun isBackStraight(): Boolean {
+        val numbers = _cards.value?.map { it % 13 }?.toList()?.sorted()
+        return numbers == intArrayOf(0, 1, 2, 3, 4).toList()
+    }
+
+    private fun isFourCards(): Boolean {
+        val numbers = _cards.value?.map { it % 13 }?.toList()?.sorted()
+        for (i in numbers!!) {
+            if (numbers.count { it == i } == 4) return true
+        }
+        return false
+    }
+
+    private fun isTriple(): Boolean {
+        val numbers = _cards.value?.map { it % 13 }?.toList()?.sorted()
+        for (i in numbers!!) {
+            if (numbers.count { it == i } == 3) return true
+        }
+        return false
+    }
+
+    private fun isTwoPair(): Boolean {
+        val numbers = _cards.value?.map { it % 13 }?.toList()?.sorted()
+        return numbers?.toSet()?.size == 3
+    }
+
+    private fun isPair(): Boolean {
+        val numbers = _cards.value?.map { it % 13 }?.toList()?.sorted()
+        return numbers?.toSet()?.size == 4
+    }
+}
--- app/src/main/java/kr/ac/kumoh/ce/s20180405/s23w04carddealer/MainActivity.kt
+++ app/src/main/java/kr/ac/kumoh/ce/s20180405/s23w04carddealer/MainActivity.kt
@@ -2,35 +2,51 @@ package kr.ac.kumoh.ce.s20180405.s23w04carddealer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+import androidx.lifecycle.ViewModelProvider
 import kr.ac.kumoh.ce.s20180405.s23w04carddealer.databinding.ActivityMainBinding
-import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
     private lateinit var main : ActivityMainBinding
-
+    private lateinit var model : CardDealerViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         main = ActivityMainBinding.inflate(layoutInflater)
         setContentView(main.root)
-        val card = Random.nextInt(52)
-        //Log.i("Test", "$card : ${getCardName(card)}")
 
-        val res = resources.getIdentifier(
-            getCardName(card),
-            "drawable",
-            packageName
-        )
+        model = ViewModelProvider(this)[CardDealerViewModel::class.java]
 
-        main.card1.setImageResource(res)
+        model.cards.observe(this) {
+            val res = IntArray(5)
+            for (i in it.indices) {
+                res[i] = resources.getIdentifier(
+                    getCardName(it[i]),
+                    "drawable",
+                    packageName
+                )
+            }
+            main.card1.setImageResource(res[0])
+            main.card2.setImageResource(res[1])
+            main.card3.setImageResource(res[2])
+            main.card4.setImageResource(res[3])
+            main.card5.setImageResource(res[4])
+        }
+
+        model.rankText.observe(this) {
+            main.ranking.text = model.rankText.value
+        }
+
+        main.btnShuffle.setOnClickListener {
+            model.shuffle()
+            model.checkRank()
+        }
     }
 
     private fun getCardName(c: Int) : String {
-        val shape = when (c / 13) {
+        var shape = when (c / 13) {
             0 -> "spades"
             1 -> "diamonds"
             2 -> "hearts"
-            3 -> "diamonds"
+            3 -> "clubs"
             else -> "error"
         }
 
@@ -41,6 +57,10 @@ class MainActivity : AppCompatActivity() {
             11 -> "queen"
             12 -> "king"
             else -> "error"
+        }
+
+        if (c % 13 >= 10) {
+            shape += "2"
         }
 
         return "c_${number}_of_${shape}"
